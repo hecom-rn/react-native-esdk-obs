644e239f8ce8277fdb883bee9969f6ef154c93a7
lzt <liuzetan1988@126.com>
2025-05-12 17:42:48 +0800

    fix(文件上传): 修改进度回调
    --bug=1056861 --user=刘泽潭 【测试】【移动端】【文档中心】上传大于1g的视频文件，上传后页面卡死了 https://www.tapd.cn/23787791/s/2079439

--- android/src/main/java/com/hecom/obs/ObsUploadManager.java
+++ android/src/main/java/com/hecom/obs/ObsUploadManager.java
@@ -37,71 +37,83 @@ public class ObsUploadManager {
     /**
      * upload
      */
-    public void upload(final ReactContext context, String bucketName, String ossFile, String sourceFile,
+    public void upload(final ReactContext context, String bucketName, String ossFile, String sFile,
                        ReadableMap options, final Promise promise) {
-        String finalSourceFile = sourceFile;
-        // Content to file:// start
-        Uri selectedVideoUri = Uri.parse(sourceFile);
-
-        // 1. content uri -> file path
-        // 2. inputstream -> temp file path
-        Cursor cursor = null;
-        try {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            cursor = context.getCurrentActivity().getContentResolver().query(selectedVideoUri, proj, null, null, null);
-            if (cursor == null) {
-                if ("file".equals(selectedVideoUri.getScheme())) {
-                    sourceFile = sourceFile.substring("file://".length());
-                } else {
-                    sourceFile = selectedVideoUri.getPath();
-                }
-            } else {
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                sourceFile = cursor.getString(column_index);
-            }
-        } catch (Exception e) {
-            sourceFile = FileUtils.getFilePathFromURI(context.getCurrentActivity(), selectedVideoUri);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-        // init upload request
-        PutObjectRequest put = new PutObjectRequest(bucketName, ossFile, new File(sourceFile));
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType("application/octet-stream");
-        put.setMetadata(metadata);
-
-        // set callback
-        put.setProgressListener(new ProgressListener() {
+        new Thread(new Runnable() {
             @Override
-            public void progressChanged(ProgressStatus progressStatus) {
-                long currentSize = progressStatus.getTransferredBytes();
-                long totalSize = progressStatus.getTotalBytes();
-                Log.d("PutObject", "currentSize: " + currentSize + " totalSize: " + totalSize);
-                String str_currentSize = Long.toString(currentSize);
-                String str_totalSize = Long.toString(totalSize);
-                WritableMap onProgressValueData = Arguments.createMap();
-                onProgressValueData.putString("currentSize", str_currentSize);
-                onProgressValueData.putString("totalSize", str_totalSize);
-                onProgressValueData.putString("totalSize", str_totalSize);
-                onProgressValueData.putString("filePath", finalSourceFile);
-                context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                        .emit("uploadProgress", onProgressValueData);
-            }
-        });
-        put.setProgressInterval(1000L);
+            public void run() {
+                long progressInterval = 1000L;
+                if (options.hasKey("fileSize")) {
+                    int fileSize = options.getInt("fileSize");
+                    if (fileSize > 100000) {
+                        progressInterval = fileSize / 100L;
+                    }
+                }
+                String tmpSourceFile = sFile;
+                String finalSourceFile = sFile;
+                // Content to file:// start
+                Uri selectedVideoUri = Uri.parse(sFile);
 
-        try {
-            PutObjectResult result = mObs.putObject(put);
-            Log.d("PutObject", "UploadSuccess");
-            Log.d("ETag", result.getEtag());
-            Log.d("RequestId", result.getRequestId());
-            promise.resolve("UploadSuccess");
-        } catch (ObsException e) {
-            PromiseExceptionManager.resolvePromiseException(e, promise);
-        }
-        Log.d("AliyunOSS", "OSS uploadObjectAsync ok!");
+                // 1. content uri -> file path
+                // 2. inputstream -> temp file path
+                Cursor cursor = null;
+                try {
+                    String[] proj = {MediaStore.Images.Media.DATA};
+                    cursor = context.getCurrentActivity().getContentResolver().query(selectedVideoUri, proj, null, null, null);
+                    if (cursor == null) {
+                        if ("file".equals(selectedVideoUri.getScheme())) {
+                            tmpSourceFile = sFile.substring("file://".length());
+                        } else {
+                            tmpSourceFile = selectedVideoUri.getPath();
+                        }
+                    } else {
+                        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                        cursor.moveToFirst();
+                        tmpSourceFile = cursor.getString(column_index);
+                    }
+                } catch (Exception e) {
+                    tmpSourceFile = FileUtils.getFilePathFromURI(context.getCurrentActivity(), selectedVideoUri);
+                } finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+                // init upload request
+                PutObjectRequest put = new PutObjectRequest(bucketName, ossFile, new File(tmpSourceFile));
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentType("application/octet-stream");
+                put.setMetadata(metadata);
+
+                // set callback
+                put.setProgressListener(new ProgressListener() {
+                    @Override
+                    public void progressChanged(ProgressStatus progressStatus) {
+                        long currentSize = progressStatus.getTransferredBytes();
+                        long totalSize = progressStatus.getTotalBytes();
+                        Log.d("PutObject", "currentSize: " + currentSize + " totalSize: " + totalSize);
+                        String str_currentSize = Long.toString(currentSize);
+                        String str_totalSize = Long.toString(totalSize);
+                        WritableMap onProgressValueData = Arguments.createMap();
+                        onProgressValueData.putString("currentSize", str_currentSize);
+                        onProgressValueData.putString("totalSize", str_totalSize);
+                        onProgressValueData.putString("totalSize", str_totalSize);
+                        onProgressValueData.putString("filePath", finalSourceFile);
+                        context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                                .emit("uploadProgress", onProgressValueData);
+                    }
+                });
+                put.setProgressInterval(progressInterval);
+                try {
+                    PutObjectResult result = mObs.putObject(put);
+                    Log.d("PutObject", "UploadSuccess");
+                    Log.d("ETag", result.getEtag());
+                    Log.d("RequestId", result.getRequestId());
+                    promise.resolve("UploadSuccess");
+                } catch (ObsException e) {
+                    PromiseExceptionManager.resolvePromiseException(e, promise);
+                }
+                Log.d("AliyunOSS", "OSS uploadObjectAsync ok!");
+            }
+        }).start();
     }
 }
